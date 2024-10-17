ff16aa1bb7120861d9e0c51221d0a80ae31cd735
hamraskannur <kkhamras741@gmail.com>
2024-10-17 16:00:35 +0530

    questions-list page created

---
+++ src/pages/judge/questions-list/QuestionsList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../../../components/navBar/NavBar";
+
+const QuestionsList = () => {
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+  );
+};
+
+export default QuestionsList;
