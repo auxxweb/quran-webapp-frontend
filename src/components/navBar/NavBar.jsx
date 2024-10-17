f9baa1476b08efe7191132fd88fbe112a4f6b5df
hamraskannur <kkhamras741@gmail.com>
2024-10-17 15:04:27 +0530

    update

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div className={styles.logoContainer}>
           <img
             className={styles.logoImage}
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+            src="/images/logo.png"
             alt="Company logo"
           />
         </div>
@@ -18,6 +18,10 @@ const NavBar = () => {
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             alt="User Profile"
           />
+          <div>
+            <h1 className={styles.text}>David Cooper</h1>
+            <h1 className={styles.text}>Calicut</h1>
+          </div>
         </div>
       </div>
     </nav>
