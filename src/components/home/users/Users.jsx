85e2337faf5495d538db9b59f1ed503eedf437af
hamraskannur <kkhamras741@gmail.com>
2024-10-17 18:08:18 +0530

    search completed

--- src/components/home/users/Users.jsx
+++ src/components/home/users/Users.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./users.module.css";
+import SearchInput from "../../search-input/SearchInput";
 
 const Users = () => {
+  const handleSearchInputChange = (value) => {
+    console.log("Search term:", value);
+  };
   return (
     <div className={styles.mainContainer}>
-      <div className="p-7 flex justify-between ">
+      <div className="p-7 px-20 flex justify-between  items-center">
         <h1 className={styles.headText}>Participants</h1>
+        <SearchInput onSearchInputChange={handleSearchInputChange} />
+        
       </div>
     </div>
   );
