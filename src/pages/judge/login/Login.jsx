1f085215a76488cc9932d1a561a3af47c2cd076c
shibil <shibi2772@gmail.com>
2024-10-17 17:21:07 +0530

    login page comleted

--- src/pages/judge/login/Login.jsx
+++ src/pages/judge/login/Login.jsx
@@ -1,75 +1,72 @@
-import styles from './Login.module.css';
+import React from "react";
+import styles from "./Login.module.css";
 
-function LoginPage() {
-    return (
-        <section className={styles.section}>
-            <div className={styles.container}>
-                <a href="#" className={styles.logo}>
-                    <img
-                        className="w-8 h-8 mr-2"
-                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                        alt="logo"
-                    />
-                    Flowbite    
-                </a>
-                <div className={styles.card}>
-                    <div className={styles.card_header}>
-                        <h1 className={styles.card_title}>
-                            Sign in to your account
-                        </h1>
-                        <form className={styles.form} action="#">
-                            <div>
-                                <label htmlFor="email" className={styles.label}>Your email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className={styles.input}
-                                    placeholder="name@company.com"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className={styles.label}>Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="••••••••"
-                                    className={styles.input}
-                                    required
-                                />
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className={styles.checkbox}>
-                                    <div className="flex items-center h-5">
-                                        <input
-                                            id="remember"
-                                            aria-describedby="remember"
-                                            type="checkbox"
-                                            className={styles.checkbox_input}
-                                            required
-                                        />
-                                    </div>
-                                    <div className={styles.checkbox_label}>
-                                        <label htmlFor="remember">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className={styles.submit_button}>
-                                Sign in
-                            </button>
-                            <p className={styles.footer_text}>
-                                Don’t have an account yet? 
-                                <a href="#" className={styles.footer_link}>Sign up</a>
-                            </p>
-                        </form>
-                    </div>
-                </div>
+function Login() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <a href="#" className={styles.logo}>
+          <img className="w-8 h-8 mr-2" src="/images/logo.png" alt="logo" />
+        </a>
+        <h1 className={styles.arabic_text}>جائزة أسلم للقرآن الكريم</h1>
+        <h1 className={styles.award_text}>ASLAM HOLY QUR’AN AWARD</h1>
+        <img className={styles.line} src="/images/login-page-line.png" alt="line" />
+        <div className={styles.card}>
+          <div className={styles.card_header}>
+            <div className={styles.card_welcome_title_div}>
+              <h1 className={styles.card_welcome_title}>Welcome Back</h1>
             </div>
-        </section>
-    );
+
+            <h1 className={styles.card_title}>Login</h1>
+            <p className={styles.card_descrption}>Glad you’re back.!</p>
+            <form className={styles.form} action="#">
+              <div>
+                <input
+                  type="text"
+                  name="Username"
+                  id="Username"
+                  className={styles.input}
+                  placeholder="Username"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  className={styles.input}
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className={styles.checkbox}>
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className={styles.checkbox_input}
+                      required
+                    />
+                  </div>
+                  <div className={styles.checkbox_label}>
+                    <label htmlFor="remember" >Remember me</label>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.submit_button_div}>
+                <button type="button" className={styles.submit_button}>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default LoginPage;
+export default Login;
