3fe3aaafe48b6bf407179d84afd615743f9ec963
shibil <shibi2772@gmail.com>
2024-10-18 16:03:51 +0530

    updates

--- src/components/participant/participant-navBar/ParticipantNavBar.jsx
+++ src/components/participant/participant-navBar/ParticipantNavBar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./ParticipantNavBar.module.css";
 
-const ParticipantNavBar = () => {
+const ParticipantNavBar = ({ children }) => {
+  const [userData, setUserData] = useState({
+    name: "David Cooper",
+    place: "Calicut Zone",
+    profileImage:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  });
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2 className={styles.WelcomeText}>
+          <img
+            className={styles.locationImage}
+            src="/images/homeLocation.png"
+            alt="location-img"
+          />
+          {userData.place}
+        </h2>
+        <div className={styles.userDetailes}>
+          <h2 className={styles.WelcomeText}>
+            <img
+              className={styles.profileImage}
+              src={userData.profileImage}
+              alt="location-img"
+            />
+            <span className={styles.nameText}>{userData.name}</span>
+          </h2>
+        </div>
+      </div>
 
-export default ParticipantNavBar
+      {children}
+    </div>
+  );
+};
+
+export default ParticipantNavBar;
--- src/pages/participant/home/Home.jsx
+++ src/pages/participant/home/Home.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 import styles from "./home.module.css";
-import ParticipantNavBar from '../../../components/participant/participant-navBar/ParticipantNavBar'
+import ParticipantNavBar from "../../../components/participant/participant-navBar/ParticipantNavBar";
+import CircularTimer from "../../../components/timer/Timer";
+import UserQuestions from "../../../components/user-questions-show/UserQuestionsShow";
 
 const Home = () => {
   return (
     <div className={styles.section}>
-    <ParticipantNavBar>
-      
-
-    </ParticipantNavBar>
+      <ParticipantNavBar>
+        <div className="h-full space-y-10">
+          <h1 className={styles.award_text}>ASLAM HOLY QUR’AN AWARD</h1>
+          <div className={styles.main_div}>
+            <CircularTimer />
+          </div>
+          <div className={styles.question_div}>
+            <UserQuestions
+              titile={"Question 1"}
+              border={"#C19D5C"}
+              descrption={"What are some other names for the Quran?"}
+            />
+          </div>
+        </div>
+      </ParticipantNavBar>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
