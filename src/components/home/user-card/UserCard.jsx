2951f6115b1a177e3ab976dd29a20c61ed764a26
Gurupriyan j <gurupriyanj007@gmail.com>
2024-10-19 13:58:00 +0530

    revert the socket changes

--- src/components/home/user-card/UserCard.jsx
+++ src/components/home/user-card/UserCard.jsx
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./UserCard.module.css";
 import UserPopup from "./user-popup/UserPopup";
-import { io } from "socket.io-client";
-import { BASE_URL } from "../../../utils/constant";
-import { useNavigate } from "react-router-dom";
-var socket;
+
 const UserCard = ({user}) => {
   const [isOpen,setIsOpen]=useState(false)
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Connect to the server
-    socket = io(BASE_URL);
-    
-    // Join the zone
-    socket.emit("join", "670e5df063e12ac02509fc9b");
-
-    // Listen for the selected-participant event
-    socket.on("selected-participant", ({ success, userId }) => {
-      console.log(userId,"userId");
-      if (success && userId) {
-        
-        // Navigate to the specific user page when the event is received
-        navigate(`/judge/current-participant/${userId}`);
-      }
-    });
-
-    return () => {
-      // Clean up the event listener when the component unmounts
-      socket.off("selected-participant");
-    };
-  }, []);
-  const handleSelectClick = () => {
-    // Emit the selected-participant event to the server
-    socket.emit("selected-participant", { success: true, userId: user?._id, zoneId: "670e5df063e12ac02509fc9b" });
-  };
   return (
     <div className={styles.mainContainer}>
       <div onClick={()=>setIsOpen(true)}  className={styles.dotsImageContainer}>
@@ -54,7 +23,7 @@ const UserCard = ({user}) => {
           </h1>
         </div>
       </div>
-     {isOpen&& <UserPopup handleSelectClick={handleSelectClick} user={user} setIsOpen={setIsOpen}/>}
+     {isOpen&& <UserPopup user={user} setIsOpen={setIsOpen}/>}
     </div>
   );
 };
