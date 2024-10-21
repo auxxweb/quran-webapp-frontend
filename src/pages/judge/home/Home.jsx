da22fd97070bbc988d73a17439ee0c37e1e60576
Gurupriyan j <gurupriyanj007@gmail.com>
2024-10-21 17:29:58 +0530

    update the judges socket issue

--- src/pages/judge/home/Home.jsx
+++ src/pages/judge/home/Home.jsx
@@ -1,31 +1,77 @@
-import React, { useState } from "react";
-import styles from "./home.module.css";
-import Users from "../../../components/home/users/Users";
-import Loading from "../../../components/loading/Loading";
-import { useAppSelector } from "../../../redux/store";
+import React, { useEffect, useState } from 'react'
+import styles from './home.module.css'
+import Users from '../../../components/home/users/Users'
+import Loading from '../../../components/loading/Loading'
+import { useAppSelector } from '../../../redux/store'
+// import { BASE_URL } from "../../utils/constant";
+import { io } from 'socket.io-client'
+import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../../../utils/constant'
+import { useHttpRequests } from '../../../api/api'
 
 const Home = () => {
-  const { judge } = useAppSelector((state) => state.judge);
+  const { judge } = useAppSelector((state) => state.judge)
+  var socket
+  const navigate = useNavigate()
+  const { get } = useHttpRequests()
+
+  useEffect(() => {
+    featchResultDetails()
+  }, [])
+
+  const featchResultDetails = async () => {
+    console.log(judge?.zoneId, 'zoneID')
+
+    const data = await get(`/judge/users/questions/zone/${judge?.zoneId}`)
+    console.log(data, 'data-------')
+    if (data && data?.data?.length > 0) {
+      navigate(
+        '/judge/question-answer/' +
+          data?.data[0]?._id +
+          '/' +
+          data?.data[0]?.currentQuestion,
+      )
+    }
+  }
+
+  useEffect(() => {
+    console.log('hello how are o', BASE_URL)
+
+    socket = io(BASE_URL)
+    socket.emit('join', judge?.zoneId)
+
+    socket.on('proceed-question', ({ success, resultId, questionId }) => {
+      console.log(resultId, 'resultId')
+      console.log(questionId, 'questionId')
+      if (success && resultId && questionId) {
+        navigate('/judge/question-answer/' + resultId + '/' + questionId)
+      }
+    })
+
+    return () => {
+      socket.off('selected-participant')
+    }
+  }, [])
 
   return (
-      <div className={styles.section}>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <h2 className={styles.WelcomeText}>Welcome</h2>
-            <h2 className={styles.WelcomeText}>
-              <img
-                className={styles.locationImage}
-                src="/images/homeLocation.png"
-                alt="location-img"
-              />
-              {judge.zone}
-            </h2>
-          </div>
-          <h1 className={styles.nameText}>{judge.name}</h1>
-          <Users />
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h2 className={styles.WelcomeText}>Welcome</h2>
+          <h2 className={styles.WelcomeText}>
+            <img
+              className={styles.locationImage}
+              src="/images/homeLocation.png"
+              alt="location-img"
+            />
+            {judge?.zone}
+          </h2>
         </div>
+        <h1 className={styles?.nameText}>{judge?.name}</h1>
+        <Users />
       </div>
-  );
-};
+    </div>
+  )
+}
 
-export default Home;
+export default Home
