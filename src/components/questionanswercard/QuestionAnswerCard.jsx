762ae11c5814ff4bc43e422ca35a55179f03c199
Gurupriyan j <gurupriyanj007@gmail.com>
2024-10-26 14:59:36 +0530

    update the arabic text issue

--- src/components/questionanswercard/QuestionAnswerCard.jsx
+++ src/components/questionanswercard/QuestionAnswerCard.jsx
@@ -5,7 +5,7 @@ import { getTextDirection } from "../../utils/constant";
 const QuestionAnswerCard = ({ titile, descrption, border }) => {
   return (
     <div className={styles.main_section}>
-      <h1 className={`${styles.titile} border-[${border}]`}>{titile}</h1>
+      <h1  className={`${styles.titile} border-[${border}]`}>{titile}</h1>
       <p className={styles.descrption} dir={getTextDirection(descrption)}>{descrption}</p>
     </div>
   );
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,5 +5,5 @@ export const BASE_URL = process.env.REACT_APP_BASE_URL
 export const socket = io.connect(BASE_URL);
 
 export const getTextDirection = (text) => {
-    return /^[\u0600-\u06FF\s]+$/.test(text) ? "rtl" : "ltr";
-  };
+  return /[\u0600-\u06FF\u0590-\u05FF]/.test(text) ? "rtl" : "ltr";
+};
